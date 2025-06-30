3aa72fd004d6050b2fd4683c31779b84f9cd4c51
BuiNguyen <nguyenbui2804@gmail.com>
2025-06-30 04:24:33 -0700

    Add counter for print debug

--- Library/debug/debug.c
+++ Library/debug/debug.c
@@ -8,15 +8,17 @@ static inline void print_char(char c)
     WRITE_OUT(c);
 }
 
-static void print_int(int64_t var, int width, int base, int sign)
+static int print_int(int64_t var, int width, int base, int sign)
 {
     int i = 0;
+    int pc = 0;
     char buf[20] = {0};
     
     if (var == 0)
     {
+        ++pc;
         print_char('0');
-        return;
+        return pc;
     }
 
     if (sign)
@@ -25,26 +27,28 @@ static void print_int(int64_t var, int width, int base, int sign)
         else sign = 0;
     }
 
-    for (; var != 0; var = var / base)
-    {
-        buf[i++] = digits[var % base];
-    }
+    for (; var != 0; var = var / base) buf[i++] = digits[var % base];
 
-    for (; i < width; ++i)
-    {
-        buf[i] = '0';
-    }
+    for (; i < width; ++i) buf[i] = '0';
 
-    if (sign) buf[i] = '-';
+    if (sign)
+    {
+        buf[i] = '-';
+        ++pc;
+    }
 
     while(--i >= 0)
     {
         print_char(buf[i]);
+        ++pc;
     }
+
+    return pc;
 }
 
-static void print_str(char* str)
+static int print_str(char* str)
 {
+    int pc = 0;
     char* null_str = "(null)";
 
     if (str == NULL)
@@ -52,19 +56,25 @@ static void print_str(char* str)
         for (; *null_str != 0; ++null_str)
         {
             print_char(*null_str);
+            ++pc;
         }
 
-        return;
+        return pc;
     }
 
     for ( ; *str != 0; ++str)
     {
         print_char(*str);
+        ++pc;
     }
+
+    return pc;
 }
 
 int debug_print(char* format, ...)
 {
+    register int pc = 0;
+    
 #ifdef DEBUG_MODE
     register int width = 0;
     va_list args;
@@ -75,6 +85,7 @@ int debug_print(char* format, ...)
         if (*format != '%')
         {
             print_char(*format);
+            ++pc;
             continue;
         }
 
@@ -85,6 +96,7 @@ int debug_print(char* format, ...)
         if (*format == '%')
         {
             print_char(*format);
+            ++pc;
             continue;
         }
 
@@ -96,30 +108,32 @@ int debug_print(char* format, ...)
 
         if (*format == 'd')
         {
-            print_int(va_arg(args, int32_t), width, 10, 1);
+            pc = print_int(va_arg(args, int32_t), width, 10, 1);
         }
         else if (*format == 'x')
         {
-            print_int(va_arg(args, int32_t), width, 16, 0);
+            pc = print_int(va_arg(args, int32_t), width, 16, 0);
         }
         else if (*format == 'X')
         {
-            print_int(va_arg(args, int32_t), width, 16, 0);
+            pc = print_int(va_arg(args, int32_t), width, 16, 0);
         }
         else if (*format == 'c')
         {
-            print_str((char[]) {
+            pc = print_str((char[]) {
                 (char) va_arg(args, int),
                 '\0'
             });
         }
         else if (*format == 's')
         {
-            print_str(va_arg(args, char*));
+            pc = print_str(va_arg(args, char*));
         }
     }
-    
-    return 0;
+
+    va_end(args);
 #endif /* DEBUG_MODE */
+
+    return pc;
 }
 
